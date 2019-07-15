cfd889202489212240d923b889fb4bd0700a9262
Namrata <namrata.sivakumar1@gmail.com>
2019-07-15 11:54:39 -0500

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../css/header.css';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="Header">
+      <ul>
+  <li><a class="active" href="#home">StackLine</a></li>
+
+</ul>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from "../header";
+import './css/styles.css';
+
+class Home extends React.Component {
+
+  render() {
+    return (
+      <div className="Home">
+<Header/>
+      </div>
+    );
+  }
+}
+
+export default Home;
